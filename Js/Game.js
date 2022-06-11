5e4569ab34def905f607f9b2d192c57ff133948e
Md. Junayed Hossain <54529889+junayed112@users.noreply.github.com>
2022-06-11 19:30:44 +0600

    Update Game.js

--- Js/Game.js
+++ Js/Game.js
@@ -1,84 +1,103 @@
 const str = ["Rock", "Paper", "Scissors"];
-var n = str.length;
-var playerSelect = document.querySelector('.playerSelect');
-var computerSelect = document.querySelector('.computerSelect');
-var player = document.querySelector('.playerScore2');
-var computer = document.querySelector('.computerScore2');
-var winnerList = document.querySelector('.winner-list');
-var winnerName = document.querySelector('.winner-name');
-var winner = document.querySelector('.winner');
-var playerScore = 0;
-var computerScore = 0;
+let n = str.length;
+const playerSelect = document.querySelector('.playerSelect');
+const computerSelect = document.querySelector('.computerSelect');
+const player = document.querySelector('.playerScore2');
+const computer = document.querySelector('.computerScore2');
+const winnerList = document.querySelector('.winner-list');
+const winnerName = document.querySelector('.winner-name');
+const winner = document.querySelector('.winner');
+const startButton = document.querySelector('.click');
+const buttons = document.querySelectorAll('.choice');
+let playerScore = 0;
+let computerScore = 0;
 player.innerHTML = playerScore;
 computer.innerHTML = computerScore;
 
-function playerSection(){
-    var input = window.prompt("Enter a string: ");
-    var output="";
-    for(let i=0;i<input.length;i++){
-        if(i==0){
-            output+= input[i].toUpperCase();
-        }
-        else{
-            output+= input[i].toLowerCase();
-        }
-    }
-    console.log(`Player = ${output}`);
-    return output;
+// document.querySelector('.option').classList.add('hidden');
+
+function Intro(){
+    // document.querySelectorAll('.choice').classList.add('hidden');
+    alert(`Welcome to the Game (Rock Paper Scissors)` );
+}
+
+function playerSection(Choice){
+    let input = Choice;
+    // let output="";
+    // for(let i=0;i<input.length;i++){
+    //     if(i==0){
+    //         output+= input[i].toUpperCase();
+    //     }
+    //     else{
+    //         output+= input[i].toLowerCase();
+    //     }
+    // }
+    // console.log(`Player = ${input}`);
+    return input;
 }
 
 function computerSection(){
-    var random = Math.floor(Math.random()*n);
-    var random_string = str[random];
-    console.log(`Computer = ${random_string}`);
+    let random = Math.floor(Math.random()*n);
+    let random_string = str[random];
+    // console.log(`Computer = ${random_string}`);
     return random_string;
 }
-// playerSection();
-// computerSection();
-var playerChoice = playerSection();
-var computerChoice = computerSection();
-playerSelect.innerHTML = playerChoice;
-computerSelect.innerHTML = computerChoice;
-if((playerChoice=="Rock" && computerChoice=="Scissors") || (playerChoice=="Paper" && computerChoice=="Rock") || (playerChoice=="Scissors" && computerChoice=="Paper")){
-    console.log(`You won! ${playerChoice} beats ${computerChoice}`);
-}
-else if(playerChoice==computerChoice){
-    console.log("This game is tied");
-}
-else{
-    console.log(`You lost! ${playerChoice} loses to ${computerChoice}`);
-}
 
-for(let j=0;j<5;j++){
-    var playerChoice = playerSection();
-    var computerChoice = computerSection();
-    playerSelect.innerHTML = playerChoice;
-    computerSelect.innerHTML = computerChoice;
-    if((playerChoice=="Rock" && computerChoice=="Scissors") || (playerChoice=="Paper" && computerChoice=="Rock") || (playerChoice=="Scissors" && computerChoice=="Paper")){
-        playerScore++;
-        console.log(`You won! ${playerChoice} beats ${computerChoice}`);
+function start(){
+    startButton.addEventListener('click', () =>{
+        document.querySelector('.click').classList.add('hidden');
+        document.querySelector('.play h2').innerHTML = "What's your Choice?";
+        document.querySelector('.option').style.display = "flex";
+    })
+}
+function stop(){
+    if(playerScore == 5 || computerScore == 5){
+        winnerList.style.display = "block";
+        winnerName.style.display = "block";
+        winnerName.innerHTML = (playerScore == 5 ? "Player Wins" : "Computer Wins" );
+        document.querySelector('.option').style.display = "none";
+        document.querySelector('.play h2').style.display = 'none';
     }
-    else if(playerChoice==computerChoice){
-        console.log("This game is tied");
+}
+
+function play(){
+    for(item of buttons){
+        item.addEventListener('click', (e)=>{
+            buttonText = e.target.innerHTML;
+            let playerChoice = playerSection(buttonText);
+            let computerChoice = computerSection();
+            playerSelect.innerHTML = playerChoice;
+            computerSelect.innerHTML = computerChoice;
+            if((playerChoice=="Rock" && computerChoice=="Scissors") || (playerChoice=="Paper" && computerChoice=="Rock") || (playerChoice=="Scissors" && computerChoice=="Paper")){
+                playerScore++;
+                document.querySelector('.play h2').innerHTML = `You won! ${playerChoice} beats ${computerChoice}`;
+                // alert(`You won! ${playerChoice} beats ${computerChoice}`);
+                // console.log(`You won! ${playerChoice} beats ${computerChoice}`);
+            }
+            else if(playerChoice==computerChoice){
+                // alert("This game is tied");
+                document.querySelector('.play h2').innerHTML = "This game is tied";
+                // console.log("This game is tied");
+            }
+            else{
+                computerScore++;
+                document.querySelector('.play h2').innerHTML = `You lost! ${playerChoice} loses to ${computerChoice}`;
+                // alert(`You lost! ${playerChoice} loses to ${computerChoice}`);
+                // console.log(`You lost! ${playerChoice} loses to ${computerChoice}`);
+            }
+            player.innerHTML = playerScore;
+            computer.innerHTML = computerScore;
+            stop();
+        })
     }
-    else{
-        computerScore++;
-        console.log(`You lost! ${playerChoice} loses to ${computerChoice}`);
-    }
-    player.innerHTML = playerScore;
-    computer.innerHTML = computerScore;
-
 }
 
-winnerList.style.display = "block";
-winnerName.style.display = "block";
-
-if(playerScore>computerScore){
-    winnerName.innerHTML = "Player Wins";
-}
-else if(playerScore<computerScore){
-    winnerName.innerHTML = "Computer Wins";
-}
-else{
-    winnerName.innerHTML = "No one";
-}
+// if(playerScore>computerScore){
+//     winnerName.innerHTML = "Player Wins";
+// }
+// else if(playerScore<computerScore){
+//     winnerName.innerHTML = "Computer Wins";
+// }
+Intro();
+start();
+play();
